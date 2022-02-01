063bef8f87e5074004b421d043d3fcccc94e5d9d
srikanth <srikantheslavath6@gmail.com>
2022-02-01 11:44:32 -0800

    All CRUD operations Done1

--- src/test/java/com/pagetests/api/TestDELETEPerson.java
+++ src/test/java/com/pagetests/api/TestDELETEPerson.java
@@ -1,4 +1,20 @@
 package com.pagetests.api;
 
+import com.pageclass.api.helpers.PersonServiceHelper;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class TestDELETEPerson {
+    private PersonServiceHelper personServiceHelper;
+
+    @BeforeClass
+    public void init(){
+        personServiceHelper = new PersonServiceHelper();
+
+    }
+    @Test
+    public void testDeletePerson(){
+        personServiceHelper.deletePerson(3);
+    }
+
 }
